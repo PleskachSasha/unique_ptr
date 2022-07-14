b8bc52ee69988df7761df60dfdfde8ce716bbed6
Oleksandr Pleskach <9pleskach9@gmail.com>
2022-07-14 15:23:31 +0300

    first commit

---
+++ Unique_ptr/Unique_ptr.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <memory>
+
+template<class T>
+class Unique_ptr {
+public:
+    
+    Unique_ptr(T* value): value_(value) {}
+
+    Unique_ptr(const Unique_ptr&) = delete;
+    Unique_ptr& operator=(const Unique_ptr&) = delete;
+
+    Unique_ptr(const Unique_ptr&& other): value_(other){
+        other = nullptr;
+    }
+    Unique_ptr& operator=(const Unique_ptr&& other) {
+        delete value_;
+        value_ = other;
+        other = nullptr;
+    }
+
+    T& operator *() {
+        return *value_;
+    }
+
+    T* operator ->() {
+        return value_;
+    }
+
+    ~Unique_ptr() {
+        delete[] value_;
+    }
+
+    T* release() { 
+        T* tmp = value_;
+        value_ = nullptr;
+        return tmp;
+    } 
+    void reset(/*T ptr = new T()*/) {
+        delete value_;
+        value_ = nullptr;
+        
+    }
+    T* get() {
+        return value_;
+    } 
+    explicit operator bool() {
+        if (value_ != nullptr)
+            return true;
+        else
+            return false;
+    } 
+
+    //T& operator[](size_t i); // if Unique_ptr store array
+
+private:
+    T* value_;
+};
+
+
+int main()
+{
+    //Unique_ptr<int> p1(new int(2));
+    std::unique_ptr<int> p1(new int(2));
+    //std::unique_ptr<int> a (std::make_unique<int>(2));
+   
+    
+    p1.reset();
+
+}
